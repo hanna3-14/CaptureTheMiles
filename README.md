592d341238490b72994eb6033ba67e244b189a1d
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-08-20 17:41:54 +0200

    added basic environments

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ my personal marathon planer including my personal race results
 
 ## formatting
 `npx prettier --write "./**/*.{js,ts,json,html}" --use-tabs --single-quote`
+
+## environments
+- `ng serve --configuration development` environment.development.ts (default)
+- `ng serve --configuration production` environment.ts
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment as env } from './../environments/environment';
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+	constructor() {
+		console.log(env.production); // Logs false for development environment
+	}
+
 	title = 'CaptureTheMiles';
 }
