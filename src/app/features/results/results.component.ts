cdad2f1844328512f9ff4a50b223636bafcee7a7
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2023-08-20 21:48:33 +0200

    added table with results

--- src/app/features/results/results.component.ts
+++ src/app/features/results/results.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Result } from 'src/app/core/models/result.model';
+import { ResultService } from 'src/app/core/services/result.service';
 
 @Component({
 	selector: 'app-results',
 	templateUrl: './results.component.html',
 })
-export class ResultsComponent {}
+export class ResultsComponent implements OnInit {
+	@Input()
+	public results: Result[] = [];
+
+	constructor(public resultService: ResultService) {}
+
+	ngOnInit(): void {
+		this.resultService.getAll().subscribe((response) => {
+			this.results = response;
+		});
+	}
+}
