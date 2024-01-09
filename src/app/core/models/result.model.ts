26833b25bad11d0b674514e77d2d4ff7d07ab95c
hanna3-14 <114485436+hanna3-14@users.noreply.github.com>
2024-01-09 19:11:53 +0100

    adapt frontend to extended results data

--- src/app/core/models/result.model.ts
+++ src/app/core/models/result.model.ts
@@ -2,6 +2,14 @@ export class Result {
 	id?: number;
 	name = '';
 	distance = '';
-	time = '';
-	place = 0;
+	timeGross = '';
+	timeNet = '';
+	category = '';
+	agegroup = '';
+	placeTotal = 0;
+	placeCategory = 0;
+	placeAgegroup = 0;
+	finisherTotal = 0;
+	finisherCategory = 0;
+	finisherAgegroup = 0;
 }
